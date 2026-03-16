38e83c24f09e9ccfdc400cc9219bf5bfcd4879a5
krishna gupta <krishna27gupta06@gmail.com>
2026-03-16 15:51:21 +0530

    KRISHNA Added UC8 Parking Lot

--- App/src/HashTablePracticeApp.java
+++ App/src/HashTablePracticeApp.java
@@ -4,14 +4,17 @@ public class HashTablePracticeApp {
 
     public static void main(String[] args) {
 
-        HashMap<String,Integer> queries = new HashMap<>();
+        HashMap<Integer,String> parking = new HashMap<>();
 
-        queries.put("java tutorial",5);
-        queries.put("java hashmap",3);
-        queries.put("java interview questions",8);
+        parking.put(1,"TN10AB1234");
+        parking.put(2,"TN09XY5678");
 
-        for(String q : queries.keySet()){
-            System.out.println(q + " -> " + queries.get(q));
+        int slot = 1;
+
+        if(parking.containsKey(slot)){
+            System.out.println("Car parked: " + parking.get(slot));
+        } else {
+            System.out.println("Slot empty");
         }
     }
 }
